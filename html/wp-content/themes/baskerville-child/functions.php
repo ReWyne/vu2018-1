72f3a92f6ac7565cc0a6cb51e8da4f0ba07c2eaf
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-05-23 16:00:22 -0500

    baskerville child remove admin css enqueueing

--- html/wp-content/themes/baskerville-child/functions.php
+++ html/wp-content/themes/baskerville-child/functions.php
@@ -3,7 +3,6 @@ function my_theme_enqueue_styles() {
 
     $parent_style = 'baskerville-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/baskerville-editor-style.css' );
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'baskerville-child-style',
         get_stylesheet_directory_uri() . '/style.css',
