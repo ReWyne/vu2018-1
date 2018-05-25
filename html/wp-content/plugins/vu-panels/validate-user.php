0ec6a6e3eb7e4c77b58989d1050fbc647c50d5c9
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-05-25 09:42:21 -0500

    validate-user speculative code; constants and boilerplate

---
+++ html/wp-content/plugins/vu-panels/validate-user.php
@@ -0,0 +1,10 @@
+<?php
+
+function validate_user($user) {
+
+    //stub #TODO (or scrap; looking into better method)
+    return true;
+
+}
+
+?>
--- html/wp-content/plugins/vu-panels/vu-panels.php
+++ html/wp-content/plugins/vu-panels/vu-panels.php
@@ -13,7 +13,37 @@ define( 'TESTING', true );
 
 global $vu_panels_vars;
 
+abstract class UserType {
+    const Admins = 'Admins';
+    const Professors = 'Faculty';
+    const Students = 'Students';
+//    const Parents = 'Parents';
+}
+
+//Collections type post -- list of links
+
+// function create_posttype() {
+//     register_post_type( 'collections',
+//     // CPT Options
+//         array(
+//             'labels' => array(
+//                 'name' => __( 'Collections' ),
+//                 'singular_name' => __( 'Collection' )
+//             ),
+//         'public' => validate_user(/*$user*/true, UserType::Professors), //#TODO
+//             'has_archive' => true,
+//             'rewrite' => array('slug' => 'movies'),
+//         )
+//     );
+// }
+
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
+
+//#temp
 add_action( 'wp_head', 'test_function' );
 function test_function() {
   echo 'vu-panels active';
 }
+
+?>
