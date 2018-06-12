2505bd8303e0ee91b57521eb2b8201aefb910736
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-06-12 15:04:23 -0500

    pcdb

--- html/wp-content/themes/vu_custom_portal/template-parts/content-link.php
+++ html/wp-content/themes/vu_custom_portal/template-parts/content-link.php
@@ -17,7 +17,7 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-		vu_log("content-link echoing $post->post_name");
+		PC::debug("content-link echoing: $post->post_name\n");
 		echo '<a class="entry-link" href="' . esc_url( get_post_meta($post->ID, "link_url_value", true) ) . '" rel="external">'.'</a>';
 		if ( 'link' === get_post_type() && is_singular() ) :
 			?>
