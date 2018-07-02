19746b2a4106d030fd13a69d2db8cbe9a6798449
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-07-02 11:24:58 -0500

    thar we go kek

--- html/wp-content/plugins/vu-panels/js/vu-admin-scripts.js
+++ html/wp-content/plugins/vu-panels/js/vu-admin-scripts.js
@@ -1,26 +1,15 @@
 //vu_alter_user_group_taxonomy submit
 $(document).ready(function(){
-	//var fso=new ActiveXObject("Scripting.FileSystemObject");
-	//var path=fso.GetAbsolutePathName(".");
-	var path = document.location.pathname;
-	console.log(path);
-	console.log('test');
-	var spath = path.split("/");
-	console.log(spath);
-	spath = spath.slice(0,-2);
-	console.log(spath);
-	spath = spath.join("/");
-	console.log(spath);
-	spath + "/wp-content/plugins/vu-panels/vu-users-permissions-ajax.php";
-	console.log(spath);
 	
     $("#vu_augt_button").click(function(){
 		var clickBtnValue = $(this).val();
-        var ajaxurl ="' . get_template_directory_uri() . '/vu-users-permissions-ajax.php",
+		var spath = document.location.pathname;
+		spath = path.split("/").slice(0,-2).join("/"); //go up two directory levels
+		spath += "/wp-content/plugins/vu-panels/vu-users-permissions-ajax.php"; //go to function's dir
 		data =  {"action": clickBtnValue,
 				 "group": $("vu_augt_group_value").val(),
 				 "role": $("#vu_augt_role_select").val()};
-        $.post(ajaxurl, data, function (response) {
+        $.post(spath, data, function (response) {
 			// Response div
 			$("#vu_augt_return").html(response["vu_augt_return"]);
 			alert("response came thru");
