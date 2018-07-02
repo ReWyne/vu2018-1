8c9c11ba21889678fa14f3b0bd26fc3b5742087a
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-07-02 12:42:17 -0500

    dbg

--- html/wp-content/plugins/vu-panels/js/vu-admin-scripts.js
+++ html/wp-content/plugins/vu-panels/js/vu-admin-scripts.js
@@ -2,13 +2,13 @@
 function vu_alter_user_group_taxonomy_submit(){
 		var clickBtnValue = $("#vu_augt_button").val();
 		var spath = document.location.pathname; //get current path
-		spath.split("/").slice(0,-2).join("/"); //go up two directory levels
+		spath = spath.split("/").slice(0,-2).join("/"); //go up two directory levels
 		spath += "/wp-content/plugins/vu-panels/vu-users-permissions-ajax.php"; //go to target function's dir
 		data =  {"action": clickBtnValue,
 				 "group": $("#vu_augt_group_field").val(),
 				 "role": $("#vu_augt_role_select").val()};
 		console.log(data);
-		console.log("vu_alter_user_group_taxonomy_submit", data, spath);
+		console.log("vu_alter_user_group_taxonomy_submit\n", data, "\n"+spath);
         $.post(spath, data, function (response) {
 			// Response div
 			$("#vu_augt_return").html(response["vu_augt_return"]);
