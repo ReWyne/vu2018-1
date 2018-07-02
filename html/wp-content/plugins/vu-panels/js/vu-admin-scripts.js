77f9874191b5b2c015b163b9b13df550ae1f5bff
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-07-02 11:35:03 -0500

    whoops

--- html/wp-content/plugins/vu-panels/js/vu-admin-scripts.js
+++ html/wp-content/plugins/vu-panels/js/vu-admin-scripts.js
@@ -1,7 +1,7 @@
 //vu_alter_user_group_taxonomy submit
 $(document).ready(function(){
 	
-    document.getElementById('button').onclick(function(){
+    document.getElementById('#vu_augt_button').onclick(function(){
 		alert ("button clicked");
 		var clickBtnValue = $(this).val();
 		var spath = document.location.pathname;
