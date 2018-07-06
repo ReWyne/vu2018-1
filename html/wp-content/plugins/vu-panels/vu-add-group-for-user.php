e74452cee3347dab847d59cc3a0d3730d1ea3e51
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-07-06 08:09:18 -0500

    whoops

--- html/wp-content/plugins/vu-panels/vu-add-group-for-user.php
+++ html/wp-content/plugins/vu-panels/vu-add-group-for-user.php
@@ -2,7 +2,7 @@
 
 defined( 'ABSPATH' ) or die(); //exit if accessed directly
 
-include_once dirname( __FILE__ ) . '/permissions.php';
+include_once dirname( __FILE__ ) . '/vu-permissions.php';
 
 
 /**
