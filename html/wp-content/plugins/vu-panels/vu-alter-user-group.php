9016686168b72e078c7edfb509f0116b4d48566e
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-07-31 10:21:28 -0500

    dbg

--- html/wp-content/plugins/vu-panels/vu-alter-user-group.php
+++ html/wp-content/plugins/vu-panels/vu-alter-user-group.php
@@ -127,7 +127,13 @@ function vu_alter_user_group_taxonomy_process_request(){
     else{
         $_POST['vu_augt_return'] = "Error: Nothing submitted";
     }
-    vu_dbg( $_POST['vu_augt_return'], get_term(3, VU_USER_GROUP) );
+    vu_dbg( $_POST['vu_augt_return'], vu_get_real_terms(array(
+         	'taxonomy' => 'vu_user_group',
+         	'hide_empty' => false,  ) ));
+        //example:
+        //get_terms( array(
+        // 	'taxonomy' => 'vu_user_group',
+        // 	'hide_empty' => false,  ) ) );
     echo json_encode($_POST['vu_augt_return']);
     wp_die();
 }
