9a5f6df9d12aca02ef2e5509ba962c399136d629
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-08-06 09:46:28 -0500

    dbg

--- html/wp-content/plugins/vu-panels/vu-util.php
+++ html/wp-content/plugins/vu-panels/vu-util.php
@@ -94,7 +94,6 @@ function vu_debug($message, $loggers = array('err_log','pc_dbg'), ...$args){
     global $vu_print_oneline_replace_text;
     if($vu_print_oneline == true){
         $output = vu_no_newlines($output, $vu_print_oneline_replace_text);
-        error_log("VU-NO-NEWLINES RESULT: ".$output);
     }
 
     //handle shorthand methods of specifying output type
