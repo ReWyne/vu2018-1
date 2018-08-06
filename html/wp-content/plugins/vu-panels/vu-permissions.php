f634298a776d49fe87ecf5976eecb701da394c37
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-08-06 08:34:30 -0500

    dbg

--- html/wp-content/plugins/vu-panels/vu-permissions.php
+++ html/wp-content/plugins/vu-panels/vu-permissions.php
@@ -377,6 +377,7 @@ function custom_post_listing($query){
     /* The current post type. */
 	$post_type = $query->get('post_type');
 	vu_dbg("got post type",$post_type);
+	vu_dbg("arr_keys",$post_type,array_keys($post_types));
 	vu_dbg("in_array check test",$post_type == 'link',$post_type == array('link'),in_array($post_type, $post_types));
     /* Check post types. */
     if(in_array($post_type, $post_types) && ($post_type == 'post' || $post_type == 'link')){
