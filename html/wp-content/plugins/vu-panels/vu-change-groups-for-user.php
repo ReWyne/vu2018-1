cf0a7a081541fc1bc2657ee426f579fb668eeafb
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-08-06 11:24:43 -0500

    dbg

--- html/wp-content/plugins/vu-panels/vu-change-groups-for-user.php
+++ html/wp-content/plugins/vu-panels/vu-change-groups-for-user.php
@@ -17,7 +17,8 @@ add_action( 'edit_user_profile', 'vu_show_extra_profile_fields' );
 add_action( 'user_new_form', 'vu_show_extra_profile_fields' );
 function vu_show_extra_profile_fields( $user ) {
 	if (!defined($user)){ //when creating a new user, we don't have this property
-		$user = ['ID' => 0];
+		$user = new StdClass;
+		$obj->ID = 0;
 	}
 	if( ! current_user_can(vu_permission_level::Admin, $user->ID)){return;}
 	?>
