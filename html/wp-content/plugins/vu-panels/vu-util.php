5d80d5c65b874a2f3d7abc83ab22f24db7762cee
ReWyne <zekken@a0-99-9b-1a-ac-7f.valpo.edu>
2018-08-07 15:26:04 -0500

    dbg

--- html/wp-content/plugins/vu-panels/vu-util.php
+++ html/wp-content/plugins/vu-panels/vu-util.php
@@ -89,9 +89,10 @@ function vu_debug($message, $loggers = array('err_log','pc_dbg'), ...$args){
     $output .= "counter: ".$vu_pc_dbg_counter;
     
     //print additional args
-    if(!empty($args)){
-        $output .= $separator.vu_to_str(...$args); //or var_export($args, true) 
+    foreach($args as $arg){
+        $output .= $separator.vu_to_str($arg); //or var_export($args, true) 
     }
+
     global $vu_print_oneline;
     global $vu_print_oneline_replace_text;
     if($vu_print_oneline == true){
