be39e67ab67078bb612368df05b4d43c413b044c
ReWyne <zekken@Anthonys-MacBook-Pro-2.local>
2018-08-09 09:12:01 -0500

    dbg

--- html/wp-content/plugins/vu-panels/vu-permissions.php
+++ html/wp-content/plugins/vu-panels/vu-permissions.php
@@ -135,6 +135,7 @@ function vu_term_exists($term, $taxonomy){
 add_action( 'admin_enqueue_scripts', 'vu_selectively_enqueue_admin_scripts' );
 function vu_selectively_enqueue_admin_scripts( $hook ) {
     if ( 'users.php' != $hook && 'profile.php' != $hook ) {
+		if(VU_RESTRICT_DEBUG_LEVEL(0))vu_dbg("vu_selectively_enqueue_admin_scripts for hook $hook failed ");
         return;
     }
 	wp_enqueue_script( 'vu_plugin_admin_js', plugins_url( '/js/vu-admin-scripts.js', __FILE__ ), array('jquery'));
