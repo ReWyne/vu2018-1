435fccb0766831565e8ef4b251568f35b05ef6c6
ReWyne <zekken@Anthonys-MacBook-Pro-2.local>
2018-08-09 09:27:54 -0500

    dbg

--- html/wp-content/plugins/vu-panels/vu-permissions.php
+++ html/wp-content/plugins/vu-panels/vu-permissions.php
@@ -134,21 +134,16 @@ function vu_term_exists($term, $taxonomy){
  */
 add_action( 'admin_enqueue_scripts', 'vu_selectively_enqueue_admin_scripts' );
 function vu_selectively_enqueue_admin_scripts( $hook ) {
-	if(VU_RESTRICT_DEBUG_LEVEL(5))vu_dbg('admin_enqueue_scripts');
-	vu_dbg($hook);
-	if(VU_RESTRICT_DEBUG_LEVEL(5))vu_dbg('admin_enqueue_scripts2');
+	if(VU_RESTRICT_DEBUG_LEVEL(3))vu_dbg('vu_selectively_enqueue_admin_scripts');
+
 	if ( 'users.php' != $hook && 'profile.php' != $hook ) {
-		if(VU_RESTRICT_DEBUG_LEVEL(5))vu_dbg("vu_selectively_enqueue_admin_scripts for hook $hook failed ");
         return;
 	}
-	if(VU_RESTRICT_DEBUG_LEVEL(5))vu_dbg("vu_selectively_enqueue_admin_scripts for hook $hook succeeded ");
-
+	vu_dbg(plugins_url( '/js/vu-admin-scripts.js', __FILE__ ));
 	wp_enqueue_script( 'vu_plugin_admin_js', plugins_url( '/js/vu-admin-scripts.js', __FILE__ ), array('jquery'));
-	vu_dbg("test3");
 	// in JavaScript, accessed as (ex) ajax_object.ajax_url
 	wp_localize_script( 'vu_plugin_admin_js', 'ajax_object',
 		array( 'ajax_url' => admin_url( 'admin-ajax.php' ), 'IS_WP_DEBUG' => (IS_WP_DEBUG === true) ) );
-	vu_dbg("test4");
 }
 
 /**
