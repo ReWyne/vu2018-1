9d469bddbef27e008766233b7eb7bafd9ba66fd9
ReWyne <zekken@Anthonys-MacBook-Pro-2.local>
2018-08-09 08:54:43 -0500

    dbg

--- html/wp-content/plugins/vu-panels/js/vu-admin-scripts.js
+++ html/wp-content/plugins/vu-panels/js/vu-admin-scripts.js
@@ -29,25 +29,8 @@ function vu_alter_user_group_taxonomy_submit(){
 	debug.log("vu_alter_user_group_taxonomy_submit\n", JSON.stringify(data), "\n"+spath);
 	$.post(spath, data, function (response) {
 		// Response div
-		$("#vu_augt_return").html(response.replace(/\\n/g, "<br />"));
+		$("#vu_augt_return").html(response.replace(/\\n/g, "<br />")); //replace \n's with <br>'s so they print properly
 	});
 };
 
-// $(document).ready(function(){
-	
-//     $("#vu_augt_button").click(function(){
-// 		alert ("button clicked");
-// 		var clickBtnValue = $(this).val();
-// 		var spath = document.location.pathname; //get current path
-// 		spath = spath.split("/").slice(0,-2).join("/"); //go up two directory levels
-// 		spath += "/wp-content/plugins/vu-panels/vu-users-permissions-ajax.php"; //go to target function's dir
-// 		data =  {"action": clickBtnValue,
-// 				 "group": $("vu_augt_group_value").val(),
-// 				 "role": $("#vu_augt_role_select").val()};
-//         $.post(spath, data, function (response) {
-// 			// Response div
-// 			$("#vu_augt_return").html(response["vu_augt_return"]);
-// 			alert("response came thru");
-//         });
-//     });
-// });
+//spath = spath.split("/").slice(0,-2).join("/"); //go up two directory levels
