db4952b12239e0d68f9092e5c73a3a38e0908533
ReWyne <zekken@Anthonys-MBP-2.lan>
2018-08-15 21:09:17 -0700

    dbg

--- html/wp-content/plugins/vu-panels/vu-filter-by-user-group.php
+++ html/wp-content/plugins/vu-panels/vu-filter-by-user-group.php
@@ -50,7 +50,7 @@ function vu_display_by_user_group_filter() {
         $output = preg_replace('/^.*>(\d+).*<\/option>.*$/', '', $output, -1, $count); // Remove lines whose printed text starts with a number (these are the reference taxonomy items)
         vu_dbg($output,"\$count = $count");
         $output = preg_replace_callback( // Replace the ID values with their slug equivalents
-            '/(?<=value=")(\d+)(?=")/g', // lookbehind & ahead
+            '/(?<=value=")(\d+)(?=")/', // lookbehind & ahead
             function ($matches) {
                 $tterm = get_term_by( 'id', $matches[0], $taxonomy );
                 return $tterm->slug;
